d48b0866d11fdf9e74bb23d5774fad1ea508b77a
Inaki Anduaga <inaki.anduaga@tado.com>
2015-09-28 00:18:19 +0200

    #1: Added quick & dirty react redux / react-redux-router implementation

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,49 @@
 /// <reference path="../node_modules/immutable/dist/immutable.d.ts" />
 
 import React = require('react');
+import { createStore, compose, combineReducers } from 'redux';
+let { Route, Link } = require('react-router');
+let {Provider, connect} = require('react-redux');
+let {ReduxRouter, routerStateReducer, reduxReactRouter} = require('redux-react-router');
+let { devTools } = require('redux-devtools');
+let { DevTools, DebugPanel, LogMonitor } = require('redux-devtools/lib/react');
+let createHistory = require('history/lib/createBrowserHistory');
+
+
 import Layout = require('./components/layout/Layout.tsx');
 let injectTapEventPlugin = require("react-tap-event-plugin");
 
 injectTapEventPlugin();
 
-React.render(
-  <Layout.Layout />,
-  document.getElementById('content')
-);
+const reducer = combineReducers({
+  router: routerStateReducer
+});
+
+const store = compose(
+  reduxReactRouter({ createHistory }),
+  devTools()
+)(createStore)(reducer);
+
+class Root extends React.Component<any, any> {
+  render() {
+    return (
+      <div>
+        <Provider store={store}>
+          { () =>
+            <ReduxRouter>
+              <Route path="/src/public/index.html" component={Layout.Layout} module="home" />
+              <Route path="/" component={Layout.Layout} module="home" >
+                <Route path="orders" component={Layout.Layout} module="orders" />
+              </Route>
+            </ReduxRouter>
+          }
+        </Provider>
+        <DebugPanel top left bottom>
+          <DevTools store={store} monitor={LogMonitor} />
+        </DebugPanel>
+      </div>
+    );
+  }
+}
+
+React.render(<Root />,document.getElementById('content'));
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="react/react.d.ts" />
 /// <reference path="node/node.d.ts" />
+/// <reference path="redux/redux.d.ts" />
